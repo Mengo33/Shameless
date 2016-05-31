c58c2cba0a6897d890093d124f0dab7879c14ef5
Mengo33 <7653576@gmail.com>
2016-05-31 19:06:16 +0300

    Added the first 3 pages: login/logout, campaign list, add new campaign.

---
+++ campaigns/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CampaignsAppConfig(AppConfig):
+    name = 'campaigns_app'
--- shameless/settings.py
+++ shameless/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'bootstrap3',
+    'django_extensions',
+
+    'campaigns',
 ]
 
 MIDDLEWARE_CLASSES = [
